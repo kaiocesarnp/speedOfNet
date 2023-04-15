a90ee14e1e23ef711369593b4d4f3b0f9a91e962
kaiocesarnp <kaiocesarnp@gmail.com>
2023-04-14 22:20:47 -0300

    velocidade da net

--- speed.py
+++ speed.py
@@ -13,33 +13,18 @@ print(output)
 
 # Explicação de cada linha:
 
-# import subprocess Esta linha importa o módulo subprocess, que permite a criação de processos 
-                                # e a comunicação com eles a partir do código Python.
+# O módulo subprocess é importado para permitir a execução de comandos no sistema operacional a partir do código Python.
+# A função test_speed() é definida para executar um teste de velocidade de internet.
+# O comando de teste de velocidade é armazenado em uma variável chamada speedtest_cmd.
+# Um processo subprocesso é criado usando subprocess.Popen(), passando o comando de teste de velocidade como argumento 
+                        # e redirecionando a saída para um objeto de tubo (pipe).
 
-# def test_speed(): Esta linha define uma função chamada test_speed(), que será usada para executar um teste de velocidade de internet.
+# A saída do processo é capturada usando o método communicate() e é atribuída à variável output. 
+# A saída do processo é retornada como uma tupla contendo a saída padrão e a saída de erro.
+# O '_' é uma convenção para descartar o valor da saída de erro, já que não está sendo usado neste código.
 
-    # speedtest_cmd = "speedtest-cli --bytes --simple" Esta linha cria uma variável chamada speedtest_cmd que armazena a string "speedtest-cli --bytes --simple". 
-                                                                    # Essa string é o comando que será passado para o processo subprocesso 
-                                                                    # para executar o teste de velocidade usando a ferramenta de linha de comando 
-                                                                    # speedtest-cli, que é uma biblioteca popular para medir a velocidade de internet em Python.
+# 'return output': Esta linha retorna a saída capturada do processo, que será o resultado da função test_speed().
 
-
-    # process = subprocess.Popen(speedtest_cmd.split(), stdout=subprocess.PIPE) Esta linha cria um objeto de processo subprocesso chamado process usando a função subprocess.Popen(). 
-                                                                                # O primeiro argumento é uma lista de strings gerada pela função split() aplicada à variável speedtest_cmd, 
-                                                                                # o que separa a string em palavras individuais. O argumento stdout=subprocess.PIPE especifica que a saída 
-                                                                                # padrão do processo (ou seja, a saída que seria impressa no terminal) deve ser redirecionada para um 
-                                                                                # objeto de tubo (pipe) para que possa ser lido pelo código Python.
-
-
-    # output, _ = process.communicate() Esta linha chama o método communicate() no objeto de processo process, que espera o processo filho 
-                                        # terminar de executar e captura sua saída. A saída do processo é retornada como uma tupla 
-                                        # contendo a saída padrão e a saída de erro. Neste caso, a saída padrão é capturada e atribuída à 
-                                        # variável output. O _ é uma convenção para descartar o valor da saída de erro, já que não está 
-                                        # sendo usado neste código.
-
-    # return output Esta linha retorna a saída capturada do processo, que será o resultado da função test_speed().
-
-# output = test_speed() Esta linha chama a função test_speed() e atribui seu resultado à variável output.
-
-# print(output) Esta linha imprime o valor da variável output, que contém a saída capturada do processo, 
-                # ou seja, o resultado do teste de velocidade de internet.
+# A função test_speed() retorna a saída capturada.
+# A função test_speed() é chamada e seu resultado é atribuído à variável output.
+# O valor da variável output é impresso na tela, que contém o resultado do teste de velocidade de internet.
